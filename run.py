da804c0b0c39374d43bcf1d277dbda82dfb36bb6
hand812 <hannahedavids@tamu.edu>
2021-02-23 18:40:30 -0600

    Changing formatting, add text
    added extra formatting and text output

--- run.py
+++ run.py
@@ -3,11 +3,11 @@ q1 = 0
 q2 = 0
 
 for line in t:
-        if '/1995' in line:
-                q1 = q1 + 1
-                
         if '/199' in line:
-                q2 = q2 + 1
+                q2 += 1
+		
+		if '/1995' in line:
+			q1 += 1
 
-print (q1)
-print (q2)
+print ("The number of transactions documented in 1995 is " , q1)
+print ("The total number of transactions documented for this period is ", q2)
