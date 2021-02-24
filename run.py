95815862f35d171682ae4367f36c06f1c40d5018
KemalBulut <78769556+KemalBulut@users.noreply.github.com>
2021-02-23 18:21:55 -0600

    Update run.py

--- run.py
+++ run.py
@@ -1,1 +1,13 @@
+t = open ('http_access_log.txt', 'r')
+q1 = 0
+q2 = 0
 
+for line in t:
+        if '/1995' in line:
+                q1 = q1 + 1
+for line in t:
+        if ',199' in line:
+                q2 = q2 + 1
+
+print (q1)
+print (q2)
